f34a5db222dddb80c1a4e5e74b80fa17095b0ee6
Baganna Kumbar <bagannak@testvagrant.com>
2023-09-06 16:08:12 +0530

    first commit

---
+++ src/main/java/org/example/AutomatingShoppingWebsite.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AutomatingShoppingWebsite {
+    public static void main(String[] args) throws InterruptedException {
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://testvagrant.myshopify.com/");
+        driver.manage().window().maximize();
+        HomePage homePage = new HomePage();
+        homePage.goToCatalogPage(driver);
+
+        CatalogPage catalogPage = new CatalogPage();
+        catalogPage.gotoProductDetailsPage(driver);
+
+        ProductDetailsPage productDetailsPage = new ProductDetailsPage();
+        productDetailsPage.addToCart(driver);
+        productDetailsPage.goToViewCartPage(driver);
+
+        ViewMyCartPage viewMyCartPage = new ViewMyCartPage();
+        viewMyCartPage.clickCheckoutButton(driver);
+
+        ShippingAddressPage shippingAddressPage = new ShippingAddressPage();
+        shippingAddressPage.addShippingAddress(driver);
+
+
+        PaymentPage paymentPage = new PaymentPage();
+        paymentPage.doPayment(driver);
+
+        driver.quit();
+
+    }
+}
